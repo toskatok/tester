4deb07928a80f305bbf0eb1817786198291b3b2e
Parham Alvani <parham.alvani@gmail.com>
2017-09-06 23:46:48 +0430

    Adds interval field into configurations.

--- config/custom-environment-variables.js
+++ config/custom-environment-variables.js
@@ -1,5 +1,6 @@
 module.exports = {
   processes: 'BAMBOO_PROCESSES',
+  interval: 'BAMBOO_INTERVAL',
   connectivity: {
     host: 'BAMBOO_CONNECTIVITY_HOST',
     port: 'BAMBOO_CONNECTIVITY_PORT'
--- config/default.js
+++ config/default.js
@@ -1,5 +1,6 @@
 module.exports = {
   processes: 2,
+  interval: 1000,
   connectivity: {
     host: '127.0.0.1',
     port: 1883
