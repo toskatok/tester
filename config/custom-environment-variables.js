a629332e0b8cb4234e23e253ca606e174b56a3b6
Parham Alvani <parham.alvani@gmail.com>
2017-09-06 23:43:36 +0430

    Adds tester required configurations.

---
+++ config/custom-environment-variables.js
@@ -0,0 +1,7 @@
+module.exports = {
+  processes: 'BAMBOO_PROCESSES',
+  connectivity: {
+    host: 'BAMBOO_CONNECTIVITY_HOST',
+    port: 'BAMBOO_CONNECTIVITY_PORT'
+  }
+}
---
+++ config/default.js
@@ -0,0 +1,7 @@
+module.exports = {
+  processes: 2,
+  connectivity: {
+    host: '127.0.0.1',
+    port: 1883
+  }
+}
