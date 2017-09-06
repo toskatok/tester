ce8eaecb2be39804d48a675d199dc9eee800c877
Parham Alvani <parham.alvani@gmail.com>
2017-09-07 00:14:48 +0430

    Completes module entry.

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const config = require('config')
 const cluster = require('cluster')
 
 if (cluster.isMaster) {
+  let components = {}
   console.log(`** Bamboo Tester ${process.pid} **`)
 
   for (let i = 0; i < config.processes; i++) {
@@ -19,13 +20,33 @@ if (cluster.isMaster) {
 
   process.on('SIGINT', () => {
     console.log('** Bamboo Tester Result **')
+    console.log(components)
     process.exit()
+  })
+
+  cluster.on('message', (worker, message, handle) => {
+    if (message.id in components) {
+      components[message.id]++
+    } else {
+      components[message.id] = 0
+      console.log(message.id)
+    }
   })
 } else {
   console.log(`> Bamboo Tester ${process.pid} started`)
+
   require('./src/tx.js')({
     host: config.connectivity.host,
     port: config.connectivity.port,
     interval: config.interval
+  })
+
+  require('./src/rx.js')({
+    host: config.connectivity.host,
+    port: config.connectivity.port
+  }, (message, id) => {
+    process.send({
+      id
+    })
   })
 }
